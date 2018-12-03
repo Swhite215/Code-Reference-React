c7a40efc87f1210d5860b32d0066d53851c05eb5
Spencer White <spencer.white@carat.com>
2018-12-03 12:35:56 -0500

    example objects style sheet

---
+++ src/featureOne/featureOne.jsx
@@ -0,0 +1,13 @@
+const featureOneStyles = () => ({
+    classOneName: {
+        marginLeft: "50px",
+        padding: "15px",
+        width: "60%"
+    },
+
+    classTwoName: {
+        width: "100px"
+    }
+});
+
+export default featureOneStyles;
