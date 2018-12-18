ae7105a3d7030f75661f340db74bc35314e34d01
Spencer White <spencer.white@carat.com>
2018-12-18 14:39:29 -0500

    Notes on global timer module api

---
+++ server/timers.js
@@ -0,0 +1,30 @@
+//Global Timer Module
+
+//Set Timeout
+let waitTime = 3000;
+
+console.log(`Wait for it...`);
+
+setTimeout(() => {
+    writeWaitingPercent(100);
+    process.stdout.write("\nDone");
+    clearInterval(myInterval);
+}, waitTime);
+
+//Set Interval
+let currentTime = 0;
+let waitInterval = 100;
+
+let myInterval = setInterval(() => {
+    currentTime += waitInterval;
+    percentWaited = Math.floor((currentTime / waitTime) * 100);
+    writeWaitingPercent(percentWaited);
+}, waitInterval);
+
+let percentWaited = 0;
+
+const writeWaitingPercent = p => {
+    process.stdout.clearLine(); //Clears last line in the terminal
+    process.stdout.cursorTo(0); //Returns cursor to first line
+    process.stdout.write(`Waiting ... ${p}%`);
+};
