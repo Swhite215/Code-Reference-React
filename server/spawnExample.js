48e7db214650abd1a3b9998280e6ab21cab1e9c8
Spencer White <spencer.white@carat.com>
2018-12-19 12:38:20 -0500

    practice basic child process

---
+++ server/spawnExample.js
@@ -0,0 +1,23 @@
+//Child Process Example
+
+let sayings = [
+    "You may delay, but time will not.",
+    "Tell me and I forget. Teach me and I remember. Involve me and I learn",
+    "It takes many good deeds to build a good reputation, only one bad one to lose it.",
+    "Early to bed and early to rise makes a man healthy, wealthy and wise.",
+    "By failing tro prepare, you are preparing to fail.",
+    "An investment in knowledge pays the best interest.",
+    "Well done is better than well said."
+];
+
+let interval = setInterval(() => {
+    let i = Math.floor(Math.random() * sayings.length);
+    process.stdout.write(`${sayings[i]} \n`);
+}, 1000);
+
+//Method to exit process when data event occurs.
+process.stdin.on("data", data => {
+    console.log(`STDIN Data Received -> ${data.toString().trim()}`);
+    clearInterval(interval);
+    process.exit();
+});
