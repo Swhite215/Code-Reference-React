bd6f065e1eb303bda53eefdfa126088d6001ccd1
Spencer White <spencer.white@carat.com>
2018-12-21 09:49:37 -0500

    added http request mocking with nock

--- server/test/tools-spec.js
+++ server/test/tools-spec.js
@@ -1,10 +1,12 @@
 //Mocha - Describe, Run and Build Tests
 //Chai - Assertion Engine, Checks Values
 //TDD - Write Test -> Code
+//Nock -
 const expect = require("chai").expect;
 const should = require("chai").should;
 const assert = require("chai").assert;
 const tools = require("./tools");
+const nock = require("nock");
 
 describe("Tools", () => {
     describe("printName()", () => {
@@ -17,8 +19,26 @@ describe("Tools", () => {
     describe("loadWiki()", () => {
         //Mocha will wait to finish test when done is invoked
         it("should load wikipedia page", done => {
+            //Providing a callback to loadWiki that will deal with response body from request
             tools.loadWiki({ first: "Abraham", last: "Lincoln" }, html => {
                 expect(html).to.be.ok;
+                done();
+            });
+        });
+    });
+
+    describe("Mock loadWiki()", () => {
+        //Hook - before will run this code to set up mocking pre-conditions
+        before(() => {
+            nock("https://en.wikipedia.org")
+                .get("/wiki/Abraham_Lincoln")
+                .reply(200, "Mock");
+        });
+
+        it("should load wikipedia page", done => {
+            //Providing a callback to loadWiki that will deal with response body from request
+            tools.loadWiki({ first: "Abraham", last: "Lincoln" }, html => {
+                expect(html).to.equal("Mock");
                 done();
             });
         });
