8686d7d22f2af00d03bf48324398362c463bd5e9
Spencer White <spencer.white@carat.com>
2019-02-22 13:40:23 -0500

    added storage engine, upload method and upload POST endpoint

--- images/app.js
+++ images/app.js
@@ -2,12 +2,36 @@ const express = require("express");
 const multer = require("multer");
 const ejs = require("ejs");
 const path = require("path");
+const { checkFileType } = require("./utilities");
 
 //Initialize App Instance
 const app = express();
 
 //EJS - Set the View Engine
 app.set("view engine", "ejs");
+
+//Set Storage Engine
+const storage = multer.diskStorage({
+    destination: "./public/uploads/",
+    //This is where we would determine Creative Name if performing operation on backend
+    filename: function(req, file, cb) {
+        cb(
+            null,
+            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+        );
+    }
+});
+
+//Set Upload - Single - Name of Input
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter: function(req, file, cb) {
+        checkFileType(file, cb);
+    }
+}).single("myImage");
 
 //Public Folder
 app.use(express.static("./public"));
@@ -19,6 +43,35 @@ app.get("/", (req, res, next) => {
 });
 
 //Deal with File Upload POST Request
+app.post("/upload", (req, res, next) => {
+    upload(req, res, err => {
+        if (err) {
+            //Rendering View and Passing Dynamic Error Variable
+            res.render("index", {
+                msg: err
+            });
+        } else {
+            if (req.file == undefined) {
+                res.render("index", {
+                    msg: "Error: No File Selected"
+                });
+            } else {
+                res.render("index", {
+                    msg: "File Uploaded",
+                    file: `uploads/${req.file.filename}`
+                });
+            }
+        }
+        // console.log(req.file); //Multer File Object Representing Image
+        // console.log(req.file.fieldname); //Field Name
+        // console.log(req.file.originalname); //Original Name
+        // console.log(req.file.encoding); //Encoding
+        // console.log(req.file.mimetype); //Mime Type
+        // console.log(req.file.destination); //Destination
+        // console.log(req.file.filename); //File Name
+        // console.log(req.file.path); //path to File
+    });
+});
 
 //Set server to listen on port
 app.listen(process.env.PORT || 3001, () =>
