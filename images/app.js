8437e3c31733ffbf2aa45d359ce652858899611f
Spencer White <spencer.white@carat.com>
2019-02-22 11:55:02 -0500

    added placeholder for POST request

--- images/app.js
+++ images/app.js
@@ -12,10 +12,13 @@ app.set("view engine", "ejs");
 //Public Folder
 app.use(express.static("./public"));
 
+//Render the Initial View
 app.get("/", (req, res, next) => {
     //Render a view using EJS
     res.render("index");
 });
+
+//Deal with File Upload POST Request
 
 //Set server to listen on port
 app.listen(process.env.PORT || 3001, () =>
