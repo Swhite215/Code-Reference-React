b912b3f37934db1660e8528539b66a25d055c100
Spencer White <swhite215@live.com>
2020-01-08 11:52:55 -0500

    example of mocha bdd interface

---
+++ testing/BDD_Interface.js
@@ -0,0 +1,24 @@
+describe('Test Suite', function() {
+  before(function() {
+    // ...
+  });
+  
+  beforeEach(function() {
+    // ...
+  });
+  
+  after(function() {
+    // ...
+  });
+  
+  afterEach(function() {
+    // ...
+  });
+
+  describe('Group()', function() {
+    context('Description', function() {
+      it('Test', function() {
+      });
+    });
+  });
+});
