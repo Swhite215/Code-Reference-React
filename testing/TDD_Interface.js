aaaef4a17909cf4ba8a4b04f465f0def0c64835e
Spencer White <swhite215@live.com>
2020-01-08 12:00:44 -0500

    tdd interface example

---
+++ testing/TDD_Interface.js
@@ -0,0 +1,22 @@
+suite('Test Suite', function() {
+  suiteSetup(function() {
+      // ... Single Execution
+  });
+  
+  setup(function() {
+      // ... Multiple Execution
+  });
+  
+  suiteTeardown(function() {
+      // ... Single Execution
+  });
+  
+  teardown(function() {
+      // ... Multiple Execution
+  });
+
+  test('Test', function() {
+  
+  });
+
+});
