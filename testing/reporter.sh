528b3fe07caaefecbf9bb91999a60f85853ead3a
Spencer White <swhite215@live.com>
2020-01-08 11:47:36 -0500

    examples reporter options

---
+++ testing/reporter.sh
@@ -0,0 +1,16 @@
+# Mocha Reporters
+mocha test --reporter spec
+mocha test --reporter dot
+mocha test --reporter nyan
+mocha test --reporter tap
+mocha test --reporter landing
+mocha test --reporter list
+mocha test --reporter progress
+mocha test --reporter json
+mocha test --reporter json-stream
+mocha test --reporter min
+mocha test --reporter doc
+mocha test --reporter markdown
+mocha test --reporter xunit
+mocha test --reporter html
+mocha test --reporter nyan
