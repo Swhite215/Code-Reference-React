49843dcb5502a1e3964491e4468a7c75d73214e5
Spencer White <swhite215@live.com>
2020-01-10 08:23:01 -0500

    example exports interface

---
+++ testing/Exports_Interface.js
@@ -0,0 +1,25 @@
+module.exports = {
+  before: function() {
+    // ... Single Execution
+  },
+  
+  beforeEach: function() {
+    // ... Multiple Execution
+  },
+  
+  after: function() {
+    // ... Single Execution
+  },
+  
+  afterEach: function() {
+    // ... Multiple Execution
+  },
+
+  TestSuite: {
+    'Description': {
+      'Test': function() {
+        // Code
+      }
+    }
+  }
+};
