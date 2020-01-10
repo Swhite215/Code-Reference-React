22950164eff39f133c834fdecc4db0df8d6c39d1
Spencer White <swhite215@live.com>
2020-01-10 08:24:43 -0500

    QUint interface example

---
+++ testing/QUint_Interface.js
@@ -0,0 +1,21 @@
+suite('Test Suite');
+
+before(function() {
+  // ... Single Execution
+});
+
+beforeEach(function() {
+  // ... Multiple Execution
+});
+
+after(function() {
+  // ... Single Execution
+});
+
+afterEach(function() {
+  // ... Multiple Execution
+});
+
+test('Test', function() {
+  // Code
+});
